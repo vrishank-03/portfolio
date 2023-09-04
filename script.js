8efd1b33031f3e37adc3b22c5d2395e55a343f98
vrishank-03 <100521118+vrishank-03@users.noreply.github.com>
2023-09-04 18:32:26 +0530

    Updated script.js 4.23

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ window.onscroll = () => {
 
 
     /*========== sticky navbar ==========*/
-
-
+    window.onscroll = () => {
+        let header = document.querySelector('.header');
+        header.classList.toggle('sticky', window.scrollY > 100);
+    };
     /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
 
 };
